6b2b64712c4ee7ab60e2046fd4094580484c468a
Taekeun Hwang <tkhwang@gmail.com>
2020-12-04 16:24:06 +0900

    Basic user module work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import * as Joi from 'joi';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { Module } from '@nestjs/common'
+import * as Joi from 'joi'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RestaurantsModule } from './restaurants/restaurants.module'
+import { Restaurant } from './restaurants/entities/restaurant.entity'
+import { UsersModule } from './users/users.module'
+import { CommonModule } from './common/common.module'
+import { User } from './users/entities/user.entity'
 
 @Module({
   imports: [
@@ -32,12 +35,14 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [Restaurant],
+      entities: [User, Restaurant],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
