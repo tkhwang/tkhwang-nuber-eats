cfeeb67d4ff38cb458891e0822c8333e23e98b03
tkhwang <tkhwang@gmail.com>
2020-12-05 17:15:56 +0900

    Add password encrption using @BeforeInsert() in User Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql'
 import { CoreEntity } from 'src/common/entities/core.entity'
-import { Column, Entity } from 'typeorm'
+import { BeforeInsert, Column, Entity } from 'typeorm'
+import * as bcrypt from 'bcrypt'
+import { InternalServerErrorException } from '@nestjs/common'
 
 // type UserRole = 'client' | 'owner' | 'delivery'
 
@@ -27,4 +29,14 @@ export class User extends CoreEntity {
   @Column({ type: 'enum', enum: UserRole })
   @Field(type => UserRole)
   role: UserRole
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10)
+    } catch (e) {
+      console.log(e)
+      throw new InternalServerErrorException()
+    }
+  }
 }
