ad1739fc9be711428e2759836a7c4bf2e620becf
tkhwang <tkhwang@gmail.com>
2020-12-05 15:45:30 +0900

    Create Account Mutation parat three

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,8 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dto/create-account.dto'
 import { User } from './entities/user.entity'
 import { UsersService } from './users.service'
 
@@ -9,5 +13,30 @@ export class UsersResolver {
   @Query(returns => Boolean)
   hi() {
     return true
+  }
+
+  @Mutation(returns => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput)
+      // error
+      if (error) {
+        return {
+          ok: false,
+          error,
+        }
+      }
+      // normal
+      return {
+        ok: true,
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      }
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { CreateAccountInput } from './dto/create-account.dto'
 import { User } from './entities/user.entity'
 
 @Injectable()
@@ -8,4 +9,23 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly user: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    // check new user
+    try {
+      const exists = await this.user.findOne({ email })
+      if (exists) {
+        // make error
+        return 'There is a user with that email already.'
+      }
+      // create user & hash the password
+      await this.user.save(this.user.create({ email, password, role }))
+    } catch (e) {
+      return "Couldn't create account"
+    }
+  }
 }
