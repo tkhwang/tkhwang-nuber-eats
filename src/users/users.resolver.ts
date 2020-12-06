786ae1aa0462df290a02e81d2a6b5d25f9582ee7
tkhwang <tkhwang@gmail.com>
2020-12-06 21:32:07 +0900

    userPfoile mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -7,6 +7,7 @@ import {
   CreateAccountOutput,
 } from './dtos/create-account.dto'
 import { LoginInput, LoginOutput } from './dtos/login.dto'
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto'
 import { User } from './entities/user.entity'
 import { UsersService } from './users.service'
 
@@ -46,5 +47,26 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User) {
     return authUser
+  }
+
+  @Query(returns => UserProfileOutput)
+  @UseGuards(AuthGuard)
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.userId)
+      if (!user) throw Error()
+
+      return {
+        ok: Boolean(user),
+        user,
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'User Not Found',
+      }
+    }
   }
 }
