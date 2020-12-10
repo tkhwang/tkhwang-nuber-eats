dd8428e9fa3afabd1a1996c484df83d830b4fe43
tkhwang <tkhwang@gmail.com>
2020-12-10 16:58:33 +0900

    [frontend] local only fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { LoggedOutRouter } from "./routers/logged-out-router"
+import { gql, useReactiveVar } from "@apollo/client"
+import { LoggedInRouter } from "./routers/logged-in-router"
+import { isLoggedInVar } from "./apollo"
+
+const IS_LOGGED_IN = gql`
+  query isLoggedIn {
+    isLoggedIn @client
+  }
+`
 
 function App() {
-  return (
-    <div className="bg-black">
-      <h1 className=" text-xl text-white">Hello TailwindCSS</h1>
-    </div>
-  )
+  const isLoggedIn = useReactiveVar(isLoggedInVar)
+
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />
 }
 
 export default App
