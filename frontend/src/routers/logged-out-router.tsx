6e012ee0ef95479837f0889ac9b9e96bf138669c
tkhwang <tkhwang@gmail.com>
2020-12-11 14:26:43 +0900

    [frontend] Login form

--- frontend/src/routers/logged-out-router.tsx
+++ frontend/src/routers/logged-out-router.tsx
@@ -1,15 +1,18 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import { CreatAccount } from "../pages/create-account"
+import { Login } from "../pages/login"
 
 export const LoggedOutRouter = () => {
   return (
     <Router>
       <Switch>
-        <Route path="create-account">
+        <Route path="/create-account">
           <CreatAccount />
         </Route>
-        <Route path="/"></Route>
+        <Route path="/">
+          <Login />
+        </Route>
       </Switch>
     </Router>
   )
